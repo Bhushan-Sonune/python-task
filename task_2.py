d5ca38e9caedad7d51f4c796621c1895f9540d50
Bhushan-Sonune <sonunebhushan2@gmail.com>
2025-06-07 11:22:55 +0530

    Delete task_2.py

--- task_2.py
+++
@@ -1,4 +0,0 @@
-first_name=str(input("Enter a first name :"))
-Last_name=str(input("Enter a last name:"))
-full_name=first_name+""+Last_name
-print("Hello,",full_name +"! welcome to python program")
