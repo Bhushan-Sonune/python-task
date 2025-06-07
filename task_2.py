b0c49e45f33cc6bcc62aeabe53a2d9a8c67d95e9
Bhushan-Sonune <sonunebhushan2@gmail.com>
2025-06-07 11:13:54 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,4 @@
+first_name=str(input("Enter a first name :"))
+Last_name=str(input("Enter a last name:"))
+full_name=first_name+""+Last_name
+print("Hello,",full_name +"! welcome to python program")
